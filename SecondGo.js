136a20a15a94b61bed82685aeeaabb56d8d1d5b9
Prince <prince@Princes-MacBook-Pro.local>
2018-09-03 16:53:00 -0400

    finished second go

--- SecondGo.js
+++ SecondGo.js
@@ -1,4 +1,29 @@
-function fourNumberSum(){
-    return [[1,2,3,4]]
+function fourNumberSum(array, target){
+    let hash = {}
+    let result = []
+    for(let first = 0; first < array.length; first ++){
+        let num1 = array[first]
+        for(let second = first + 1; second < array.length; second++){
+            let num2 = array[second]
+            let sum = num2 + num1
+            let diff = target - sum
+            if(hash[diff]){
+                for(let i = 0; i < hash[diff].length; i++){
+                    result.push((hash[diff][i].concat([num1,num2])))
+                }
+            }
+        }
+        for(let m = 0; m < first; m++){
+            let current = array[m]
+            let summed = current + num1
+            if(hash[summed]){
+                hash[summed].push([current,num1])
+            }else {
+                hash[summed] = [[current,num1]]
+            }
+        }
+    }
+    return (result)
+
 }
 exports.fourNumberSum = fourNumberSum
